c2c92697c22b804e3a0c5d8bf7c557609b59686d
vietng322611 <vietng322611@gmail.com>
2025-12-07 15:33:43 +0700

    feat: add `Friends` route

--- app/src/main/java/com/example/minisocialnetworkapplication/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/minisocialnetworkapplication/ui/navigation/Screen.kt
@@ -7,6 +7,7 @@ sealed class Screen(val route: String) {
     data object ComposePost : Screen("compose_post")
     data object Settings : Screen("settings")
     data object Chat : Screen("chat")
+    data object Friends : Screen("friends")
     data object SearchUser: Screen("search")
     data object PostDetail : Screen("post_detail/{postId}") {
         fun createRoute(postId: String) = "post_detail/$postId"
